7a7bded7b0ac7bc4e4c5c64a98e13ed062241af4
keamogetsw3 <keamogetswegp@gmail.com>
2023-08-24 06:13:20 -0700

    config

---
+++ monty.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+
+/**
+ * main - The entry point for the Monty program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * Return: 0 on success, or an error code on failure.
+ */
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+    return (0)
+
+}
